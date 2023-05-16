162b726aab492c232bdc205fd9567dd269ef8d0d
Adriano_Araujo <adriano.rodrigues365@outlook.com>
2023-05-15 22:18:14 -0300

    Add desenvolvimento web css

--- CADASTROCLIENTE/views.py
+++ CADASTROCLIENTE/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from CADASTROCLIENTE.models import Cliente, Profissao
+from CADASTROCLIENTE.models import Cliente, Profissao, Telefone
 # Create your views here.
 def index(request):
     meu_nome = "Beltrano da Costa"
@@ -36,8 +36,11 @@ def lista_Profissoes(request):
 def detalhar_cliente(request, id):
     #Buscando no banco de dados o cliente pelo id
     cliente = Cliente.objects.get(id = id)
+    telefones = Telefone.objects.filter(cliente_id = id)
+
     context = {
-        "cliente": cliente
+        "cliente": cliente,
+        "telefones": telefones,
     }
     return render(request, "cliente_detalhes.html", context)
     
